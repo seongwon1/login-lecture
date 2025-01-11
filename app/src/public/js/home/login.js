67898901ed6c8faba748ae31b208f001b255c43f
unknown <tlrhdtk123@naver.com>
2025-01-11 16:09:00 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,6 +11,13 @@ function login(){
         id: id.value,
         psword: psword.value
     };
-console.log(req);
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req)
+
+    });
 };
 
