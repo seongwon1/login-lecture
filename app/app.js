1e7d7cc5575275ae3fd83f2fd8abd7ae480a664c
unknown <tlrhdtk123@naver.com>
2025-01-11 14:39:27 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// 모듈
+const express = require("express");
+const app = express();
+
+// 라우팅
+const home = require("./src/routes/home");
+
+// 앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.use("/",home); // use -> 미들웨어를 등록해주는 메서드.
+module.exports = app;
+
+
+
+
+
+
+
+
+
+// const http = require("http");
+// const app = http.createServer((req,res) =>{
+//     res.writeHead(200,{"Content-Type": "text/html; charset=utf-8"}); //200은 정상응답 코드를 보내줌
+//     if(req.url == '/'){
+//         res.end("여기는 루트입니다.")
+//     } else if(req.url === "/login"){
+//         res.end("여기는 로그인 화면입니다.")
+//     };
+// });
+// app.listen(3001, ()=>{
+//     console.log("http 가동서버");
+// });
